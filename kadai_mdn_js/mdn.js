a53f6003c470c689639595586b6e41e3e64cb0cc
YoshinoriKiyota <157315602+YoshinoriKiyota@users.noreply.github.com>
2024-02-10 01:31:29 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,11 @@
 const today = new Date();
 
 const date = () => {
-    console.log(today.getFullYear() + '年');
-    console.log((today.getMonth() + 1) + '月');
-    console.log(today.getDate() + '日');
+   const year = today.getFullYear();
+   const month = today.getMonth() + 1;
+   const day = today.getDate();
+
+   console.log(year + '年' + month + '月' + day + '日');
 }
 
 date();
