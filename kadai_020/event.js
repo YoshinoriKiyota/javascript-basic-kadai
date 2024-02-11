fa3b89754e7cc35ee267d557644b6669a3767527
YoshinoriKiyota <157315602+YoshinoriKiyota@users.noreply.github.com>
2024-02-12 05:59:17 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const addText = document.getElementById('text');
+const addBtn = document.getElementById('btn');
+
+addBtn.addEventListener('click', () => {
+
+    addText.textContent = 'ボタンをクリックしました';
+    
+})
